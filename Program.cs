a52ef483c9c08728b8ba4240ec06b37c32e38870
AliHazem3 <147556529+AliHazem3@users.noreply.github.com>
2024-06-05 20:33:50 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -31,36 +31,6 @@ namespace C__Studing
 
 
 
-        /*
-        static int max(int[] arr)
-        {
-
-            int counter = 0;
-            int maxcounter = 0;
-            int mostsecti = 0;
-            for (int i = 1; i < arr.Length; i++)
-            {
-                if (arr[i] != arr[i - 1])
-                {
-                    counter = 0;
-                }
-                else
-                {
-                    counter++;
-                }
-                
-                if(counter > maxcounter)
-                {
-                    maxcounter = counter;
-
-                    mostsecti = arr[i];
-                }
-                    
-            }
-
-            return mostsecti;
-        }
-        
 
 
 
@@ -68,64 +38,8 @@ namespace C__Studing
 
 
 
-        /*
-        static int GetMaxNumber(int[] arr)
-        {
-            if (arr == null || arr.Length == 0)
-            {
-                throw new ArgumentException("The array is null or empty.");
-            }
 
-            int max = arr[0];
-            for (int i = 1; i < arr.Length; i++)
-            {
-                if (arr[i] > max)
-                {
-                    max = arr[i];
-                }
-            }
-
-            return max;
-        }
-
-
-        ###############
-
-        class Sort
-        {   
-
-            public void sort(int[] arr)
-            {
-                int n = arr.Length;
-                // One by one move boundary of unsorted subarray 
-                for (int i = 0; i < n - 1; i++)
-                {
-                    // Find the minimum element in unsorted array 
-                    int min_idx = i;
-                    for (int j = i + 1; j < n; j++)
-                        if (arr[j] < arr[min_idx])
-                            min_idx = j;
-
-                    // Swap the found minimum element with the first 
-                    // element 
-                    int temp = arr[min_idx];
-                    arr[min_idx] = arr[i];
-                    arr[i] = temp;
-                }
-            }
-
-            // Prints the array 
-            public void printArray(int[] arr)
-            {
-                int n = arr.Length;
-                for (int i = 0; i < n; ++i)
-                    Console.Write(arr[i] + " ");
-                Console.WriteLine();
-            }
-        }
-
-         */
-
+    
 
 
 
